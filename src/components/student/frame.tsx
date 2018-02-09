96753229a64a86ceea8e547e454e4e48f5aa93b9
Jonah Turnquist <jturnquist@c02w103chtdg.office.atlassian.com>
2018-02-09 12:05:52 +1100

    screen sharing

--- src/components/student/frame.tsx
+++ src/components/student/frame.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Table, Checkbox, Button, Icon, Modal, Form, Header, Image, Input } from 'semantic-ui-react';
+import {Table, Checkbox, Button, Icon, Modal, Form, Header, Image, Input} from 'semantic-ui-react';
 
 import Util from '../../utils/util';
 
-import { OTSession, OTPublisher, OTStreams, OTSubscriber } from 'opentok-react';
+import {OTSession, OTPublisher, OTStreams, OTSubscriber} from 'opentok-react';
 
 declare var Pusher: any;
 declare var config: RemoteConfig;
@@ -21,6 +21,9 @@ export interface FrameState {
 }
 
 export class Frame extends React.Component<FrameProps, FrameState> {
+    private subscriberEventHandlers: any;
+    private onSubscribe: (event: any) => any;
+
     constructor(props: FrameProps) {
         super(props);
 
@@ -29,52 +32,72 @@ export class Frame extends React.Component<FrameProps, FrameState> {
             teacherSharing: false
         };
 
-        OT.getDevices((err, devices) => {
-            const audioDevices = devices.filter(device => device.kind === 'audioInput');
-
-            OT.getUserMedia({ audioSource: audioDevices[0].deviceId, videoSource: null });
-
-            this.setState({ audioDeviceId: audioDevices[0].deviceId });
+        OT.getUserMedia({
+            videoSource: null
+        }).then((micStream) => {
+            console.log('getUserMedia fulfilled')
+            this.setState({audioDeviceId: micStream.getAudioTracks()[0]})
         });
+
+        // OT.getDevices((err, devices) => {
+        //     const audioDevices = devices.filter(device => device.kind === 'audioInput');
+        //
+        //     OT.getUserMedia({ audioSource: audioDevices[0].deviceId, videoSource: null });
+        //
+        //     this.setState({ audioDeviceId: audioDevices[0].deviceId });
+        // });
+
+        this.subscriberEventHandlers = {
+            destroyed: (event: any) => {
+                this.setState({teacherSharing: false})
+            }
+        };
+        this.onSubscribe = (event: any) => {
+            this.setState({teacherSharing: true})
+        }
     }
 
     render() {
-        const { url } = this.props;
-        const { audioDeviceId, teacherSharing } = this.state;
+        const {url} = this.props;
+        const {audioDeviceId, teacherSharing} = this.state;
 
         return <div className="student-view">
-            {
-                teacherSharing ?
-                    <div>TEACHER IS SHARING</div> :
-                    null
-            }
-
-            <OTSession apiKey={session.opentok_api_key} sessionId={session.opentok_teacher_session_id} token={session.opentok_teacher_token}>
-                <OTStreams>
-                    <OTSubscriber properties={
-                        { 
-                            width: 800,
-                            height: 600,
+            <div className="student-publisher">
+                <OTSession apiKey={session.opentok_api_key} sessionId={session.opentok_session_id}
+                           token={session.opentok_token}>
+                    {audioDeviceId ?
+                        <OTPublisher properties={{
+                            publishVideo: true,
+                            publishAudio: true,
+                            width: 100,
+                            height: 100,
+                            videoSource: 'screen',
+                            audioSource: audioDeviceId
+                        }}/>
+                        : null}
+                </OTSession>
+            </div>
+            <div className="student-subscriber">
+                <OTSession apiKey={session.opentok_api_key} sessionId={session.opentok_teacher_session_id}
+                           token={session.opentok_teacher_token}>
+                    <OTStreams>
+                        <OTSubscriber properties={{
+                            width: "100%",
+                            height: "100%",
                             subscribeToAudio: true,
                             subscribeToVideo: true,
                             audioVolume: 100
-                        }
-                    }
-                    eventHandlers={
-                        {
-                            videoEnabled: () => this.setState({ teacherSharing: true }),
-                            videoDisabled: () => this.setState({ teacherSharing: false })
-                        }
-                    }
-                    />
-                </OTStreams>
-            </OTSession>
-
-            <OTSession apiKey={session.opentok_api_key} sessionId={session.opentok_session_id} token={session.opentok_token}>
-                <OTPublisher properties={{ publishVideo: true, publishAudio: true, width: 100, height: 100, videoSource: 'screen', audioSource: audioDeviceId }} />
-                
-                <iframe id="content-iframe" src={url} sandbox="allow-top-navigation allow-scripts allow-same-origin"></iframe>
-            </OTSession>
+                        }}
+                                      eventHandlers={this.subscriberEventHandlers}
+                                      onSubscribe={this.onSubscribe}
+                        />
+                    </OTStreams>
+                </OTSession>
+            </div>
+            {teacherSharing ? null :
+                <iframe id="content-iframe" src={url}
+                        sandbox="allow-top-navigation allow-scripts allow-same-origin"></iframe>
+            }
         </div>;
     }
 }
